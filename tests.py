121d84127e5872ddf0dffa8ad1a3c1a93ae4298e
Julien Malik <julien.malik@gmail.com>
2014-12-12 15:37:59 +0100

    [test] experiment

--- tests.py
+++ tests.py
@@ -2,17 +2,29 @@
 
 import os
 
-def test1():
-  print 'inside test1'
-  print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), 'LICENSE'))
-  assert( 1 == 1 )
+class YunoTest
 
-def test2():
-  print 'inside test2'
-  print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), 'single_app_test.py'))
-  assert( 1 == 2 )
+  def attach_file( relative_path ):
+    output_path = os.path.join(os.path.dirname(__file__), 'YunoTest')
+    try:
+      os.makedirs( output_path )
+    except os.error:
+      pass
 
-def test3():
-  print 'inside test3'
-  assert( 1 == 3 )
+    os.system('cp %s %s' % ( os.path.join(os.getcwd(), relative_path), output_path)
 
+  def test1(self):
+    print 'inside test1'
+    self.attach_file('LICENSE')
+#    print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), ))
+    assert( 1 == 1 )
+
+  def test2(self):
+    print 'inside test2'
+    self.attach_file('single_app_test.py')
+#    print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), 'single_app_test.py'))
+    assert( 1 == 2 )
+
+  def test3(self):
+    print 'inside test3'
+    assert( 1 == 3 )
