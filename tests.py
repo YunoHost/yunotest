593ecc4f08e7e45b14a001f405e9ce0ec31230cf
Julien Malik <julien.malik@gmail.com>
2014-12-12 16:01:05 +0100

    [test] experiment

--- tests.py
+++ tests.py
@@ -2,29 +2,28 @@
 
 import os
 
-class YunoTest
-
-  def attach_file( relative_path ):
-    output_path = os.path.join(os.path.dirname(__file__), 'YunoTest')
+class TestYunoapps:
+  def attach_file(self, relative_path):
+    output_path = os.path.join(os.path.dirname(__file__), self.__class__.__name__)
     try:
       os.makedirs( output_path )
     except os.error:
       pass
 
-    os.system('cp %s %s' % ( os.path.join(os.getcwd(), relative_path), output_path)
+    command = 'cp %s %s' % ( os.path.join(os.getcwd(), relative_path), output_path)
+    print command
+    os.system( command )
 
-  def test1(self):
+  def test_1(self):
     print 'inside test1'
     self.attach_file('LICENSE')
-#    print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), ))
     assert( 1 == 1 )
 
-  def test2(self):
+  def test_2(self):
     print 'inside test2'
     self.attach_file('single_app_test.py')
-#    print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.dirname(__file__), 'single_app_test.py'))
     assert( 1 == 2 )
 
-  def test3(self):
+  def test_3(self):
     print 'inside test3'
     assert( 1 == 3 )
