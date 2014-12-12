4de7162ee6f4a1bb3f2c4044b1277e0c47f7cfb5
Julien Malik <julien.malik@gmail.com>
2014-12-12 14:41:04 +0100

    [test] experiment

--- tests.py
+++ tests.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 def test1():
   print 'inside test1'
+  print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.basename(__file__), 'LICENSE'))
   assert( 1 == 1 )
 
 def test2():
   print 'inside test2'
+  print '[[ATTACHMENT|%s]]' % (os.path.join( os.path.basename(__file__), 'single_app_test.py'))
   assert( 1 == 2 )
 
 def test3():
