3ef1221a88410f4326c36445ecf7d88e7d5406b4
Julien Malik <julien.malik@gmail.com>
2014-12-16 17:31:10 +0100

    [enh] add dependency for agendav

--- apps_tests/__init__.py
+++ apps_tests/__init__.py
@@ -94,6 +94,9 @@ def _make_AppTest(config):
 
       def test_install(self):
         global context
+        for depends in config["install_depends"]:
+          (command_output, exitstatus) = context.server.install_app(configs.configlist[depends])
+          assert exitstatus == 0
         (command_output, exitstatus) = context.server.install_app(config)
         self.attach_data(command_output, "install.txt")
         assert exitstatus == 0
