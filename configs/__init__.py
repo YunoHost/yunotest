f57c588240be0a671bcab0e5a145f532b8e4190f
Julien Malik <julien.malik@gmail.com>
2014-12-21 23:06:39 +0100

    [test] tmp

--- configs/__init__.py
+++ configs/__init__.py
@@ -13,5 +13,8 @@ def init():
       test_prop = {}
       with open(os.path.join(thisdir, json_filename)) as json_file:
         configlist[app] = json.loads(str(json_file.read()))
+  
+  # temporary test
+  configlist = { "baikal" : configlist["baikal"] }
 
 init()
