ec77605c6a625e649ac4d7f2415796310627e19a
Julien Malik <julien.malik@gmail.com>
2014-12-22 11:28:46 +0100

    [test] remove tmp hack

--- configs/__init__.py
+++ configs/__init__.py
@@ -15,7 +15,4 @@ def init():
       with open(os.path.join(thisdir, json_filename)) as json_file:
         configlist[app] = json.loads(str(json_file.read()))
   
-  # temporary test
-  configlist = { "baikal" : configlist["baikal"] }
-
 init()
